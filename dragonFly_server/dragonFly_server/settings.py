16451fb9c58d6eb3799f0092b724d75d1fb8c4ef
JeongHeumChoi <andyc707@gmail.com>
2023-10-17 20:15:55 +0900

    FEAT : MySQL settings

--- dragonFly_server/dragonFly_server/settings.py
+++ dragonFly_server/dragonFly_server/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'dragonFly_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'    : 'django.db.backends.mysql',
+        'NAME'      : get_env_variable('DB_NAME'),
+        'USER'      : get_env_variable('DB_USER'),
+        'PASSWORD'  : get_env_variable('DB_PASSWORD'),
+        'HOST'      : get_env_variable('DB_HOST'),
+        'PORT'      : get_env_variable('DB_PORT'),
     }
 }
 
