e4fe2299b753a92b4fdf5af584d17303bdbe246c
JeongHeumChoi <andyc707@gmail.com>
2023-12-23 17:40:12 +0900

    feat: settings.py 분리에 따른 실행파일명 변경

--- dragonFly_server/dragonFly_server/asgi.py
+++ dragonFly_server/dragonFly_server/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dragonFly_server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dragonFly_server.settings.prod')
 
 application = get_asgi_application()
